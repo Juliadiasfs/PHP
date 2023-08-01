b02831253c0f97dd960c07121d6c6acffcf77f66
Juliadiasfs <117773552+Juliadiasfs@users.noreply.github.com>
2023-08-01 16:45:36 -0300

    Add files via upload

--- meu_primeiro_site_php/index.php
+++ meu_primeiro_site_php/index.php
@@ -10,7 +10,16 @@ $x= 10;
 $y= 10.5;
 echo  "<br>" . $x + $y;
 
-?>
+// You can also use comments to leave out parts of a code line
+$x = 5 /* + 15 */ + 5;
+echo "<br>". $x;
 
+$cars = array("Volvo", "BMW", "Toyota");
+var_dump($cars);
+for($i=0; $i<3; $i++){
+echo "<br> carro na posição ".$i." ". $cars[$i];
+}
+echo "<br>". strlen("Julia Dias Ferreira da Silva");
+?>
 </body>
 </html>
